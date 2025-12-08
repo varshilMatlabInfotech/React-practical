29e2898aa35b6c151d9ea9b61a18b1e8dadd0c49
test <testingmatlabinfotech1@gmail.com>
2025-12-08 16:39:41 +0530

    search Ui done

--- src/pages/home/Index.tsx
+++ src/pages/home/Index.tsx
@@ -1,6 +1,7 @@
 import { AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -13,18 +14,52 @@ import { bookmarkCheck } from "@/store/user/userSlice";
 import type { USERI } from "@/types/Index";
 import { Avatar } from "@radix-ui/react-avatar";
 import { Bookmark } from "lucide-react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const Index = () => {
+  // const itemsPerPage = 10;
+  // const [itemOffset, setItemOffset] = useState(0);
   const { data } = useSelector((s: RootState) => s.user);
   const dispatch = useDispatch<AppDispatch>();
+  const [search, setSearch] = useState<string>("");
+  // const [filterData, setFilterData] = useState<USERI[]>(data);
 
   const handleBookMarkCheck = (arg: USERI) => {
     dispatch(bookmarkCheck(arg));
   };
 
+  // const endOffset = itemOffset + itemsPerPage;
+
+  // const currentItems = data.slice(itemOffset, endOffset);
+  // const pageCount = Math.ceil(data.length / itemsPerPage);
+
+  // const handlePageClick = (event: number) => {
+  //   const newOffset = (event * itemsPerPage) % data.length;
+  //   setItemOffset(newOffset);
+  // };
+
+  const handleSearch = (event: any) => {
+    setTimeout(() => {
+      setSearch(event.target.value);
+    }, 500);
+  };
+
+  // useEffect(() => {
+  //   if (search) {
+  //     const res: USERI[] = data?.filter((item) => item?.login === search);
+  //     console.log("filterDatafilterDatafilterData", filterData);
+  //     setFilterData(res);
+  //   } else {
+  //     setFilterData(data);
+  //   }
+  // }, [search]);
+
   return (
     <div className="min-h-screen w-full px-10 py-10">
+      <div className="mt-2 mb-5">
+        <Input placeholder="Type here..." onChange={(e) => handleSearch(e)} />
+      </div>
       <Table>
         <TableHeader className="bg-gray-100">
           <TableCell>ID</TableCell>
@@ -77,6 +112,28 @@ const Index = () => {
           )}
         </TableBody>
       </Table>
+      {/* <div className="w-full px-10 mt-10">
+        <Pagination className="w-full flex justify-end">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              {[...Array(pageCount)].map((_, idx) => (
+                <PaginationLink
+                  onClick={() => handlePageClick(idx + 1)}
+                  href="#"
+                >
+                  {idx + 1}
+                </PaginationLink>
+              ))}
+            </PaginationItem>
+            <PaginationItem onClick={() => handlePageClick(2)}>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div> */}
     </div>
   );
 };
